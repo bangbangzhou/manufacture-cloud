761b1caaa1fcfa72b18aabd27c6c65c3d3ae62a5
Administrator <825505212@qq.com>
2021-12-18 15:32:28 +0800

    put project

---
+++ manufacture-register/src/main/java/com/manufacture/ManufactureRegisterApplication.java
@@ -0,0 +1,13 @@
+package com.manufacture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManufactureRegisterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManufactureRegisterApplication.class, args);
+    }
+
+}
---
+++ manufacture-register/src/test/java/com/manufacture/ManufactureRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manufacture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManufactureRegisterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
